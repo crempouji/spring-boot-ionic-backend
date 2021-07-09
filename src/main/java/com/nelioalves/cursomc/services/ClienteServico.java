de3375d38b84bc54622fe97f58cff9fba580b72d
crempouji <gabriel.crempe@usp.br>
2021-07-09 11:28:04 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/nelioalves/cursomc/services/ClienteServico.java
@@ -0,0 +1,24 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteServico {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Cliente não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+}
